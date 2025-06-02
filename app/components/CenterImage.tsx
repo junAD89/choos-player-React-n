c22e5dce98b2dd5a59cc58626c8469e86c3c50c2
junAD89 <balhighlight@gmail.com>
2025-06-02 10:49:53 +0000

    final

--- app/components/CenterImage.tsx
+++ app/components/CenterImage.tsx
@@ -1,16 +1,41 @@
 import { Image, StyleSheet, View } from "react-native";
+import Swiper from 'react-native-swiper';
 
 export default function CenterImage() {
     return (
         <View style={styles.container}>
-            <Image
-                source={{
-                    uri: `https://fifpro.org/media/cvglel2t/imago1020346080.jpg?rxy=0.47221054333764556,0.1990534726514335&width=1600&height=1024&rnd=133226681627000000`
-                }}
-                style={styles.image}
-            />
 
 
+
+
+            <Swiper>
+
+                <Image
+                    source={{
+                        uri: `https://fifpro.org/media/cvglel2t/imago1020346080.jpg?rxy=0.47221054333764556,0.1990534726514335&width=1600&height=1024&rnd=133226681627000000`
+                    }}
+                    style={styles.image}
+                />
+                <View>
+
+                    <Image
+                        source={{
+                            uri: `https://fifpro.org/media/cvglel2t/imago1020346080.jpg?rxy=0.47221054333764556,0.1990534726514335&width=1600&height=1024&rnd=133226681627000000`
+                        }}
+                        style={styles.image}
+                    />
+                </View>
+                <View>
+
+
+                    <Image
+                        source={{
+                            uri: `https://fifpro.org/media/cvglel2t/imago1020346080.jpg?rxy=0.47221054333764556,0.1990534726514335&width=1600&height=1024&rnd=133226681627000000`
+                        }}
+                        style={styles.image}
+                    />
+                </View>
+            </Swiper>
         </View>
     )
 }
